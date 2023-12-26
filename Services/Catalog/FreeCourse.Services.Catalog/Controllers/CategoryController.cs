e606ce582c8d1082fea900d0cc5e08180d4712c0
Emre ÖZGÖZ <emreozgoz@gmail.com>
2023-12-26 14:53:24 +0300

    feat : category controller added

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _categoryService.GetAllAsync();
+            return CreateActionResultBase(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            var response = await _categoryService.CreateAsync(category);
+            return CreateActionResultBase(response);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _categoryService.GetByIdAsync(id);
+            return CreateActionResultBase(response);
+        }
+    }
+}
